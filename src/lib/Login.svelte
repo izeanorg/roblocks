8820070195f8515ea7e1d2af1828c0203bea0e09
izeanorg <izeanorg@mac.com>
2022-07-18 01:54:08 -0700

    1 file modified

--- src/lib/Login.svelte
+++ src/lib/Login.svelte
@@ -3,14 +3,15 @@
     <h3>EMAIL LOGIN / SIGN UP</h3>
   </label>
   <input>
-    <button>GO</button>
-    <h3>CURRENTLY LIMITED TO INVITE ONLY</h3>
+  <button>GO</button>
+  <h3>CURRENTLY LIMITED TO INVITE ONLY</h3>
 </form>
 <style>
 input {
   display:flex;
   flex-direction:column;
   flex:1;
+  width:97%;
 }
 label {
   display: flex;
