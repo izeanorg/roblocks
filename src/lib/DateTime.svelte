66e72bd6fe407771913ab39d84e03cb70b42d4bf
izeanorg <izeanorg@mac.com>
2022-07-19 17:54:05 -0700

    1 file added

---
+++ src/lib/DateTime.svelte
@@ -0,0 +1,12 @@
+<script>
+  import { onMount } from 'svelte';
+
+  let time = new Date();
+
+  $: hours = time.getHours();
+  $: minutes = time.getMinutes();
+  $: seconds = time.getSeconds();
+
+  onMount(() => {<->});
+
+</script>
