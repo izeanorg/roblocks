0ea7ce678d3c54e62fd62d6d5f7142b101e166eb
izeanorg <izeanorg@mac.com>
2022-07-19 20:20:33 -0500

    1 file modified

--- src/lib/header/Header.svelte
+++ src/lib/header/Header.svelte
@@ -1,5 +1,6 @@
 <script>
 	import { page } from '$app/stores';
+	import time from '$lib/Time.svelte';
 </script>
 
 
@@ -13,6 +14,7 @@
 			<li class:active={$page.url.pathname === '/todos'}>
 				<a sveltekit:prefetch href="/todos">Blocks</a>
 			</li>
+			{time}
 			<!-- USE FOR STATS PAGE <li class:active={$page.url.pathname === '/stats'}>
 							<a sveltekit:prefetch href="/stats">Stats</a>
 </li> -->
